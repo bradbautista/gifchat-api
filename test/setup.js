a7cb6aa6e1aa4f844f2d6efda49fa5ef24725221
unknown <bradbautista@gmail.com>
2020-01-15 17:13:41 -0500

    cleaned up code

--- test/setup.js
+++
@@ -1,7 +0,0 @@
-process.env.TZ = 'UTC'
-require('dotenv').config()
-const { expect } = require('chai')
-const supertest = require('supertest')
-
-global.expect = expect
-global.supertest = supertest
