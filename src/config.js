4fb5f79e40e6d31a580bc0e07d697bf74d1531ba
unknown <bradbautista@gmail.com>
2020-01-16 01:05:28 -0500

    cleaned up code, dirs

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
   PORT: process.env.PORT || 17043,
-  DATABASE_URL: process.env.DATABASE_URL || 'postgresql://dunder_mifflin:password@localhost/blogful',
-  TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://dunder_mifflin:password@localhost/blogful-test'
+  DATABASE_URL: process.env.DATABASE_URL || 'postgresql://dunder_mifflin:password@localhost/noteful_db',
+  TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://dunder_mifflin:password@localhost/noteful_db-test'
 }
