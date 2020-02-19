b31aaae99cb115d2e9dd58e0a7d15cf790eb56c4
unknown <bradbautista@gmail.com>
2020-02-18 21:15:27 -0500

    removed unused code

--- src/config.js
+++ src/config.js
@@ -1,9 +1,5 @@
 module.exports = {
   PORT: process.env.PORT || 17043,
-  DATABASE_URL:
-    process.env.DATABASE_URL ||
-    'postgresql://postgres:0m8x34s3@localhost/gifchat_db',
-  TEST_DATABASE_URL:
-    process.env.TEST_DATABASE_URL ||
-    'postgresql://postgres:0m8x34s3@localhost/gifchat_db-test'
+  DATABASE_URL: process.env.DATABASE_URL,
+  TEST_DATABASE_URL: process.env.TEST_DATABASE_URL
 };
